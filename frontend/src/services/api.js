b86057e889cc6211b59af7e884f6ef0b6c6ce752
bharathikannansambasivam <bharathikannansambasivam@gmail.com>
2026-04-18 14:16:12 +0530

    chore: update API baseURL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://newsletter-campaign-system-1.onrender.com",
+  baseURL: "https://api.newslettercam.me",
   withCredentials: true,
 });
 
